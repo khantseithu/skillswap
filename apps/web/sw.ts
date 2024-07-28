e73ae548a5093cb2f742801f7753f42e6c6eceaf
Khant Sithu <khantseithuu@gmail.com>
2024-07-28 16:38:25 +0000

    chore: Update service worker configuration

--- apps/web/sw.ts
+++ apps/web/sw.ts
@@ -1,5 +1,4 @@
-// @ts-ignore
-import { defaultCache } from "@serwist/next/browser";
+import { defaultCache } from "@serwist/next/worker";
 import type { PrecacheEntry } from "@serwist/precaching";
 import { installSerwist } from "@serwist/sw";
 
